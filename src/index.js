10b56192e9ef5e9412b8158eb00d77d32b90cc98
Anna-fog <anna49.fog@yandex.ru>
2021-02-06 23:04:50 +0300

    feat: implement task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(inputString, bracketsConfig) {
+
+    if (inputString.length % 2 > 0) {
+        return false;
+    }
+
+    let inputStringArray = Array.from(inputString);
+
+    while (true) {
+
+        let isMatch = false;
+        for (let i = 0; i < inputStringArray.length; i++) {
+            for (let config of bracketsConfig) {
+                if (inputStringArray[i] === config[0] && inputStringArray[i + 1] === config[1]) {
+                    inputStringArray.splice(i, 2);
+                    isMatch = true;
+                    break;
+                }
+            }
+            if (isMatch) {
+                break;
+            }
+        }
+        if (inputStringArray.length === 0) {
+            return true;
+        }
+        if (!isMatch) {
+            return false;
+        }
+    }
 }
